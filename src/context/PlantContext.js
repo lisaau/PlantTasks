74baf3927bc7736109dd70f62c67c9e56e1ec67e
lisaau <lisaau@users.noreply.github.com>
2020-04-18 14:08:59 -0700

    Example API call [WIP]

--- src/context/PlantContext.js
+++ src/context/PlantContext.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const PlantContext = React.createContext();
 
 
 export const PlantProvider = ({ children }) => {
-    const plants = [
-        { id: 1, name: 'Plant #1', species: 'Air plant' },
-        { id: 2, name: 'Plant #2', species: 'tulip' },
-    ]
+    const [plants, setPlants] = useState([]);
+    const fetchPlants = async () => {
+        try {
+          const plants = await fetch('https://127.0.0.1:8000/plants')
+          setPlants(plants)
+          console.log(plants)
+        } catch (e) {
+          if (e) {
+            console.log(e.message, 'Something went wrong')
+          }
+        }
+    }
+
+    useEffect(() => {
+        fetchPlants()
+    }, [])
+
+    // const plants = [
+    //     { id: 1, name: 'Plant #1', species: 'Air plant' },
+    //     { id: 2, name: 'Plant #2', species: 'tulip' },
+    // ]
 
     return <PlantContext.Provider value={plants}>
         {children}
