4141bed405826493c7aa3544b65612f1b4fbb79b
lisaau <lisaau@users.noreply.github.com>
2020-04-20 16:55:58 -0700

    Add addPlant endpoint

--- src/context/PlantContext.js
+++ src/context/PlantContext.js
@@ -1,34 +1,56 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const PlantContext = React.createContext();
 
-
 export const PlantProvider = ({ children }) => {
-    // const [plants, setPlants] = useState([]);
-    // const fetchPlants = async () => {
-    //     try {
-    //       const plants = await fetch('https://127.0.0.1:8000/plants')
-    //       setPlants(plants)
-    //       console.log(plants)
-    //     } catch (e) {
-    //       if (e) {
-    //         console.log(e.message, 'Something went wrong')
-    //       }
-    //     }
-    // }
+    const [plants, setPlants] = useState([]);
+    const fetchPlants = async () => {
+        try {
+            const apiPlants = await fetch(
+                'https://planttasks.herokuapp.com/plants'
+            );
+            const json = await apiPlants.json();
+            setPlants(json);
+        } catch (e) {
+            if (e) {
+                console.log(e.message, 'Something went wrong');
+            }
+        }
+    };
 
-    // useEffect(() => {
-    //     fetchPlants()
-    // }, [])
+    const addNewPlant = async ({ name, species }) => {
+        try {
+            const apiPlants = await fetch(
+                '/https://planttasks.herokuapp.com/plant',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({ name, species })
+                }
+            );
+            const json = await apiPlants.json();
+            console.log(json);
+            setPlants([...plants, json]);
+        } catch (e) {
+            if (e) {
+                console.log(e.message, 'Something went wrong');
+            }
+        }
+    };
 
-    const plants = [
-        { id: 1, name: 'Plant #1', species: 'Air plant' },
-        { id: 2, name: 'Plant #2', species: 'tulip' },
-    ]
+    useEffect(() => {
+        fetchPlants();
+    }, []);
 
-    return <PlantContext.Provider value={plants}>
-        {children}
-    </PlantContext.Provider>
+    // const plants = [
+    //     { id: 1, name: 'Plant #1', species: 'Air plant' },
+    //     { id: 2, name: 'Plant #2', species: 'tulip' },
+    // ]
+
+    return (
+        <PlantContext.Provider value={{ plants, addNewPlant }}>
+            {children}
+        </PlantContext.Provider>
+    );
 };
 
 export default PlantContext;
--- src/screens/ViewPlantScreen.js
+++ src/screens/ViewPlantScreen.js
@@ -3,10 +3,10 @@ import { Text, View, Button } from 'react-native';
 import PlantContext from '../context/PlantContext';
 
 export default function ViewPlantScreen({ navigation, route }) {
-    const plants = useContext(PlantContext)
-    console.log(route.params.id)
-    const plant = plants.find( p => p.id === route.params.id)
-    
+    const { plants } = useContext(PlantContext);
+    console.log(route.params.id);
+    const plant = plants.find(p => p.id === route.params.id);
+
     return (
         <View
             style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
