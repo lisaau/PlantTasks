9016901542b2085e6bee63480795e5ab19c9ced6
lisaau <lisaau@users.noreply.github.com>
2020-04-20 19:17:14 -0700

    Fix addNewPlant name

--- src/screens/CreatePlantScreen.js
+++ src/screens/CreatePlantScreen.js
@@ -4,14 +4,14 @@ import PlantForm from '../components/PlantForm';
 import PlantContext from '../context/PlantContext';
 
 export default function CreatePlantScreen({ navigation }) {
-    const { addPlant } = useContext(PlantContext);
-
+    const { addNewPlant } = useContext(PlantContext);
+    console.log(addNewPlant)
     return (
         <View
             style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
         >
             <Text>Form to add a plant</Text>
-            <PlantForm navigation={navigation} onSave={addPlant} />
+            <PlantForm navigation={navigation} onSave={addNewPlant} />
         </View>
     );
 }
