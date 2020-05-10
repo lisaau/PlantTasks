73471a32393fbc9a792cbb62efc1787b03c151c6
lisaau <lisaau@users.noreply.github.com>
2020-05-09 23:36:00 -0700

    Add DismissKeyboard and reduce width of dropdown

--- src/screens/TaskFormScreen.js
+++ src/screens/TaskFormScreen.js
@@ -1,13 +1,15 @@
-import React, { useState, useContext } from 'react';
+import React from 'react';
 import { Text, View, Button, TextInput, StyleSheet } from 'react-native';
-import RNPickerSelect from 'react-native-picker-select';
 import { EvilIcons } from '@expo/vector-icons';
+import RNPickerSelect from 'react-native-picker-select';
+
 import TaskContext from '../context/TaskContext';
+import DismissKeyboard from '../components/DismissKeyboard';
 
 export default function TaskFormScreen({ navigation, route }) {
-    const { addNewTask } = useContext(TaskContext);
-    const [description, setDescription] = useState('');
-    const [frequency, setFrequency] = useState('');
+    const { addNewTask } = React.useContext(TaskContext);
+    const [description, setDescription] = React.useState('');
+    const [frequency, setFrequency] = React.useState('');
     const plantId = route.params.id;
 
     // frequency values for RNPickerSelect
@@ -24,37 +26,39 @@ export default function TaskFormScreen({ navigation, route }) {
     });
 
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Add description:</Text>
-            <TextInput
-                value={description}
-                onChangeText={text => setDescription(text)}
-                style={styles.input}
-                placeholder='required'
-            />
-            <Text>Frequency (in days):</Text>
-            <RNPickerSelect
-                style={pickerSelectStyles}
-                Icon={() => {
-                    return <EvilIcons name='chevron-down' style={{fontSize: 44}} />;
-                }}
-                onValueChange={(value) => setFrequency(value)}
-                items={daysSelection}
-            />
-            <Button
-                onPress={() => {
-                    if (description === '') {
-                        alert('Please enter the description of the task')
-                    } else if(frequency === null || frequency === '') {
-                        alert('Please enter number of days');
-                    } else {
-                        addNewTask(description, frequency, plantId)
-                    }
-                    navigation.goBack()
-                }}
-                title="Save Task"
-            />
-    </View>
+        <DismissKeyboard>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={styles.text}>Add description:</Text>
+                <TextInput
+                    value={description}
+                    onChangeText={text => setDescription(text)}
+                    style={styles.input}
+                    placeholder='required'
+                />
+                <Text style={styles.text}>Frequency (in days):</Text>
+                <RNPickerSelect
+                    style={pickerSelectStyles}
+                    Icon={() => {
+                        return <EvilIcons name='chevron-down' style={styles.icon} />;
+                    }}
+                    onValueChange={(value) => setFrequency(value)}
+                    items={daysSelection}
+                />
+                <Button
+                    onPress={() => {
+                        if (description === '') {
+                            alert('Please enter the description of the task')
+                        } else if(frequency === null || frequency === '') {
+                            alert('Please enter number of days');
+                        } else {
+                            addNewTask(description, frequency, plantId)
+                        }
+                        navigation.goBack()
+                    }}
+                    title="Save Task"
+                />
+        </View>
+    </DismissKeyboard>
     );
 }
 
@@ -65,32 +69,45 @@ const styles = StyleSheet.create({
         borderColor: 'black',
         marginBottom: 15,
         padding: 5,
+    },
+    text: {
         margin: 5,
     },
+    icon: {
+        fontSize: 44,
+        marginRight: "10%",
+        marginTop: "5%"
+    }
 });
 
 const pickerSelectStyles = StyleSheet.create({
     inputIOS: {
-      fontSize: 16,
-      paddingVertical: 12,
-      paddingHorizontal: 10,
-      borderWidth: 1,
-      borderColor: 'gray',
-      borderRadius: 4,
-      color: 'black',
-      paddingRight: 30, // to ensure the text is never behind the icon
-      textAlign: 'center'
+        fontSize: 16,
+        paddingVertical: 12,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        borderRadius: 4,
+        color: 'black',
+        paddingRight: 30, // to ensure the text is never behind the icon
+        textAlign: 'center',
+        marginLeft: "10%",
+        marginRight: "10%",
+        marginBottom: "10%"
     },
     inputAndroid: {
-      fontSize: 16,
-      paddingHorizontal: 10,
-      paddingVertical: 8,
-      borderWidth: 0.5,
-      borderColor: 'purple',
-      borderRadius: 8,
-      color: 'black',
-      paddingRight: 30, // to ensure the text is never behind the icon
-      textAlign: 'center'
+        fontSize: 16,
+        paddingHorizontal: 10,
+        paddingVertical: 8,
+        borderWidth: 0.5,
+        borderColor: 'purple',
+        borderRadius: 8,
+        color: 'black',
+        paddingRight: 30, // to ensure the text is never behind the icon
+        textAlign: 'center',
+        marginLeft: "10%",
+        marginRight: "10%",
+        marginBottom: "10%"
     },
   });
   
