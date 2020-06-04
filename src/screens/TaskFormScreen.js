4f320fc44a645b29762ba9a5ed7a5ff1616937b5
lisaau <lisaau@users.noreply.github.com>
2020-06-03 22:58:29 -0700

    Extract form field validation into separate function

--- src/screens/TaskFormScreen.js
+++ src/screens/TaskFormScreen.js
@@ -18,10 +18,21 @@ export default function TaskFormScreen({ navigation, route }) {
     daysSelection.push({ label: i.toString(), value: i });
   }
 
+  const formFieldsValidator = () => {
+    if (description === '') {
+      alert('Please enter the description of the task');
+    } else if (frequency === null || frequency === '') {
+      alert('Please enter number of days');
+    } else {
+      addNewTask(description, frequency, plantId);
+      navigation.goBack();
+    }
+  };
+
   React.useLayoutEffect(() => {
     navigation.setOptions(
       {
-        title: `Add Task for ${route.params.name}`
+        title: `Add Task for ${route.params.name}`,
       },
       [navigation]
     );
@@ -33,13 +44,13 @@ export default function TaskFormScreen({ navigation, route }) {
         style={{
           flex: 1,
           justifyContent: 'center',
-          alignItems: 'center'
+          alignItems: 'center',
         }}
       >
         <Text style={styles.text}>Add description:</Text>
         <TextInput
           value={description}
-          onChangeText={text => setDescription(text)}
+          onChangeText={(text) => setDescription(text)}
           style={styles.input}
           multiline={true}
           maxLength={140}
@@ -51,22 +62,10 @@ export default function TaskFormScreen({ navigation, route }) {
           Icon={() => {
             return <EvilIcons name="chevron-down" style={styles.icon} />;
           }}
-          onValueChange={value => setFrequency(value)}
+          onValueChange={(value) => setFrequency(value)}
           items={daysSelection}
         />
-        <Button
-          onPress={() => {
-            if (description === '') {
-              alert('Please enter the description of the task');
-            } else if (frequency === null || frequency === '') {
-              alert('Please enter number of days');
-            } else {
-              addNewTask(description, frequency, plantId);
-              navigation.goBack();
-            }
-          }}
-          title="Save Task"
-        />
+        <Button onPress={formFieldsValidator} title="Save Task" />
       </View>
     </DismissKeyboard>
   );
@@ -79,16 +78,16 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     marginBottom: '10%',
     padding: 5,
-    width: '80%'
+    width: '80%',
   },
   text: {
-    margin: 5
+    margin: 5,
   },
   icon: {
     fontSize: 44,
     marginRight: '10%',
-    marginTop: '5%'
-  }
+    marginTop: '5%',
+  },
 });
 
 const pickerSelectStyles = StyleSheet.create({
@@ -104,7 +103,7 @@ const pickerSelectStyles = StyleSheet.create({
     textAlign: 'center',
     marginLeft: '10%',
     marginRight: '10%',
-    marginBottom: '10%'
+    marginBottom: '10%',
   },
   inputAndroid: {
     fontSize: 16,
@@ -118,6 +117,6 @@ const pickerSelectStyles = StyleSheet.create({
     textAlign: 'center',
     marginLeft: '10%',
     marginRight: '10%',
-    marginBottom: '10%'
-  }
+    marginBottom: '10%',
+  },
 });
